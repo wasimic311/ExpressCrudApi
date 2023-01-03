73ecef51eacd2aed6334404706f821c53b8699e1
Wasim <wasim.ic311@gmail.com>
2023-01-03 20:31:49 +0100

    Adding async await to method post at the enpoint /posts

--- routes/posts.js
+++ routes/posts.js
@@ -9,19 +9,14 @@ router.get('/', (req, res) => {
 });
 
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
     const post = new Post({
         title: req.body.title,
         description: req.body.description
     });
 
-    post.save()
-    .then(data => {
-        res.json(data);
-    })
-    .catch(err => {
-        res.json({message: err});
-    })
+    const savedPost = await post.save()
+    res.json(savedPost);
 });
 
 
