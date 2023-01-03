261ef75599d29838a93caf0a3b36ce276e802461
Wasim <wasim.ic311@gmail.com>
2023-01-03 05:22:54 +0100

    Connection with the database MongoDB

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
+require('dotenv/config');
+mongoose.set('strictQuery', true);
 const app = express();
 
 
-//Routes
 
+//Routes
 app.get('/', (req, res) => {
 
     res.send('Hello world');
 });
 
+app.get('/posts', (req, res) => {
+
+    res.send('Posts');
+});
+
+//Connect to DB
+mongoose.connect(process.env.DB_CONNECTION, () => {
+    console.log('Connected to DB!');
+});
 
 //Start listening
 app.listen(3000);
