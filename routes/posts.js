4e0c3637c8c8219f63abb79d5cc38af4dc7d2492
Wasim <wasim.ic311@gmail.com>
2023-01-03 21:25:38 +0100

    Update a post method using PATCH

--- routes/posts.js
+++ routes/posts.js
@@ -33,8 +33,19 @@ router.post('/', async (req, res) => {
         description: req.body.description
     });
     try{
-    const savedPost = await post.save()
+    const savedPost = await post.save();
     res.json(savedPost);
+    }
+    catch(err){
+        res.json({message: err});
+    }
+});
+
+//UPDATE A POST
+router.patch('/:postId', async (req, res) => {
+    try{
+    const updatedPost = await Post.updateOne({_id: req.params.postId}, { $set: { title: req.body.title, description: req.body.description } });
+    res.json(updatedPost);
     }
     catch(err){
         res.json({message: err});
